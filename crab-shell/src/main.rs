480d3f0e5dc6d8f9d787a0e0d4b8d846e5b1a789
Folgue02 <davidfolru@gmail.com>
2022-05-04 20:25:57 +0200

    New default prompt implemented to crab-shell

--- crab-shell/src/main.rs
+++ crab-shell/src/main.rs
@@ -1,8 +1,12 @@
+use colored::Colorize;
 use crab_shell_core::*;
+use std::boxed;
+use std::env;
+use whoami;
 
 fn main() {
     let mut s = shell::Shell::new();
-    //s.prompt_handler = std::boxed::Box::new(random_handler);
+    s.prompt_handler = boxed::Box::new(prompt_handler);
     loop {
         let user_input = s.prompt_user();
         let code = s.execute_user_input(command::parse_input(user_input).unwrap());
@@ -15,6 +19,16 @@ fn main() {
 }
 
 // Testing stuff
-fn random_handler() -> String {
-    "TEST ::: ".to_string()
+fn prompt_handler() -> String {
+    format!(
+        "[{}@{} {}] ",
+        whoami::username().green(),
+        whoami::hostname().blue(),
+        env::current_dir()
+            .unwrap()
+            .to_str()
+            .unwrap()
+            .to_string()
+            .bright_blue()
+    )
 }
